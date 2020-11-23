b9224f4f0cd249b01be6f59a0dd1e7712f589dad
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-11-22 18:40:15 -0800

    tests pass

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,43 @@
 class Instructor
+    attr_reader :name
+    
+    @@all = [] 
+    def initialize(name)
+        @name = name 
+        @@all << self 
+    end 
 
+    def self.all
+        @@all
+    end 
+
+    def tests
+        BoatingTest.all.select {|test| test.instructor == self}
+    end 
+
+    def passed_students 
+        passed = tests.select {|test| test.status == "passed"}
+        passed.map {|x| x.student}
+    end 
+
+    def pass_student(student, boating_test)
+        student_instance = tests.select {|test| test.student == student}
+        student_test = student_instance.find {|test| test.name == boating_test}
+        if student_test 
+            student_test.status = "passed"
+            student_test
+        else         
+            BoatingTest.new(student, boating_test, self, status = "passed")
+        end
+    end
+
+    def failed_students 
+        passed = tests.select {|test| test.status == "failed"}
+        passed.map {|x| x.student}
+    end 
+
+    def all_students(name)
+        final = tests.select {|test| test.name == name}
+        final.map {|x| x.student}
+    end 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,37 @@
 class Student
+    attr_reader :first_name
+
+    @@all = [] 
+    def initialize(first_name)
+        @first_name = first_name 
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def add_boating_test(boating_test, status, instructor)
+        BoatingTest.new(self, boating_test, status, instructor)
+    end 
+
+    def tests 
+        BoatingTest.all.select {|test| test.student == self}
+    end 
+
+    def all_instructors 
+        tests.map {|test| test.instructor}.uniq
+    end 
+
+    def self.find_student(first_name)
+        Student.all.find {|student| student.first_name == first_name}
+    end 
+
+    def grade_percentage
+        test_count = tests.count.to_f
+        pass_count = tests.select {|test| test.status == "passed"}.count.to_f
+        passed = (pass_count / test_count)*100
+        passed
+    end 
 
 end
